f1973449a821b1f16a41fd07db6e72a40dc67048
Nicole Sitenok <57601862+sitenok@users.noreply.github.com>
2021-04-16 23:46:32 +0100

    code
    Q1 = Creating & Outputting strings
    Q2 = More string functions

---
+++ lab4/Q1.c
@@ -0,0 +1,22 @@
+/* What do the following printf() statements do?
+    Strings and how to output them
+    Date: 27/02/2020
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    //printf("%5s", "abcd");
+    
+    //printf("%5s", "abcdef");
+    
+    //printf("%-5s", "abc");
+    
+    //printf("%5.2s", "abcde");
+    
+    printf("%-5.2s", "abcde");
+    
+    getchar();
+    return 0;
+}
---
+++ lab4/Q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[] = {'R', 'o', 'b', 'e', 'r', 't'};
+
+    puts( name );
+
+    scanf( "%s", "Philip");
+
+    strcpy( name, "Philip" );
+
+    *(name + 5) = "a";
+
+    name = "Philip";
+
+    getchar();
+    return 0:
+
+}
